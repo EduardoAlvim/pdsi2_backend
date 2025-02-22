b5bb9c80fe12f6956f7b0ca2656f777d7c9cb0c6
Eduardo Alvim <eduardo25@ufu.br>
2025-02-22 02:46:22 -0300

    aula 10

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ import classes
 import model
 from database import engine, get_db
 from sqlalchemy.orm import Session
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+ 'http://localhost:3000'
+]
+app.add_middleware(
+ CORSMiddleware,
+ allow_origins=origins,
+ allow_credentials=True,
+ allow_methods=['*'],
+ allow_headers=['*']
+)
+
 @app.get("/")
 def read_root():
  return {"Hello": "lala"}
@@ -16,6 +30,11 @@ def read_root():
 @app.get("/quadrado/{num}")
 def square(num: int):
  return num ** 2
+
+@app.get("/mensagens", response_model=List[classes.Mensagem], status_code=status.HTTP_200_OK)
+async def buscar_valores(db: Session = Depends(get_db), skip: int = 0, limit: int=100):
+ mensagens = db.query(model.Model_Mensagem).offset(skip).limit(limit).all()
+ return mensagens
 
 @app.post("/criar", status_code=status.HTTP_201_CREATED)
 def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)):
